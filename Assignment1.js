5edbb06b8837d420cc9db754a9b30b5386554314
Likitha <likithadevegowda2396@gmail.com>
2022-02-27 13:43:57 +0100

    Push functionality complete

--- Assignment1.js
+++ Assignment1.js
@@ -8,4 +8,15 @@ class Stack {
         this.stackTop = -1;
         this.stack_size = stack_size;
     }
+    push(num) {
+        if (this.stackTop == this.stack_size - 1) {
+            console.log("upper limit of stack reached, cannot insert");
+        }
+        else {
+            this.stackTop = this.stackTop + 1;
+            this.array[this.stackTop] = num;
+            console.log(`The element being pushed is ${num}`);
+            return this.stackTop;
+        }
+    }    
 }
